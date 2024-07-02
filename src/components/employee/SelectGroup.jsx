1cd4b46120f89c8add1b817b1e1572ab6e5396f8
Giorgi Kalandadze <g.kalandadze@gorgia.ge>
2024-07-02 09:58:16 +0400

    feat/makes select menu and create employee page

---
+++ src/components/employee/SelectGroup.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import CheckIcon from "../../assets/Check.png"; 
+
+const SelectGroup = ({ label, options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (option) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="w-full flex flex-col gap-2">
+      <label className="text-[#105D8D] font-medium">{label}</label>
+      <div className="relative inline-block w-full">
+        <div
+          className="border border-[#105D8D] py-3 rounded-2xl px-2 cursor-pointer flex justify-between items-center"
+          onClick={toggleDropdown}
+        >
+          <span className={`text-gray-400 ${selected ? "text-black" : ""}`}>
+            {selected ? selected : label}
+          </span>
+          <svg
+            className={`w-4 h-4 transition-transform transform ${
+              isOpen ? "rotate-180" : "rotate-0"
+            }`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </div>
+        {isOpen && (
+          <div className="absolute w-full mt-1 border border-[#105D8D] rounded bg-white shadow-lg z-10">
+            {options.map((option, index) => (
+              <div
+                key={index}
+                className="p-2 hover:bg-gray-200 cursor-pointer flex justify-between items-center"
+                onClick={() => handleSelect(option)}
+              >
+                {option}
+                {selected === option && (
+                  <div className="ml-2 bg-[#1976D2] rounded p-1">
+                    <img src={CheckIcon} alt="Checked" className="w-4 h-4" />
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SelectGroup;
--- src/pages/Employee/Create/EmployeeCreate.jsx
+++ src/pages/Employee/Create/EmployeeCreate.jsx
@@ -6,70 +6,125 @@ import NewIcon from '../../../assets/new.png'
 import SaveIcon from '../../../assets/save.png'
 import DeleteIcon from '../../../assets/delete.png'
 import EditIcon from '../../../assets/edit.png'
+import SelectGroup from '../../../components/employee/SelectGroup'
 
 const EmployeeCreate = () => {
   return (
     <AuthenticatedLayout>
-        <div className='w-full px-20 py-4 flex flex-col gap-8'>
-            <div className='flex justify-between w-full'>
-                <h1 className='text-[#1976D2] font-medium text-[23px]'>თანამშრომლის დამატება/ცვლილება</h1>
-                <button className='bg-[#1976D2] px-7 py-4 rounded-2xl'>
-                    <img src={CloseIcon}/>
-                </button>
-            </div>
-            <div className='flex justify-end gap-4'>
-                <button className='bg-[#5CB85C] text-white px-4 py-2 rounded-md flex items-center gap-2'><img src={NewIcon}/>New</button>
-                <button className='bg-[#1976D2] text-white px-4 py-2 rounded-md flex items-center gap-2'><img src={EditIcon}/>Edit</button>
-                <button className='bg-[#D9534F] text-white px-4 py-2 rounded-md flex items-center gap-2'><img src={DeleteIcon}/>Delete</button>
-                <button className='bg-[#FBD15B] text-white px-4 py-2 rounded-md flex items-center gap-2'><img src={SaveIcon}/>Save</button>
-            </div>
-            <div className='flex flex-col gap-4'>
-                <div className='flex justify-between gap-8'>
-                    <InputGroup
-                        label="სახელი/გვარი"
-                        name="fullname"
-                        placeholder="სახელი/გვარი"
-                        type="text"
-                    />
-                    <InputGroup
-                        label="პირადი ნომერი / ID"
-                        name="personal_id"
-                        placeholder="პირადი ნომერი"
-                        type="number"
-                    />
-                </div>
-                <div className='flex justify-between gap-8'>
-                    <InputGroup
-                        label="ტელეფონის ნომერი"
-                        name="phone_number"
-                        placeholder="ტელეფონის ნომერი"
-                        type="number"
-                    />
-                    <InputGroup
-                        label="დეპარტამენტი"
-                        name="personal_id"
-                        placeholder="დეპარტამენტი"
-                        type="number"
-                    />
-                </div>
-                <div className='flex justify-between gap-8'>
-                    <InputGroup
-                        label="დაწყების დრო"
-                        name="start_date"
-                        placeholder="დაწყების დრო"
-                        type="date"
-                    />
-                    <InputGroup
-                        label="გათავისუფლების დრო"
-                        name="end_date"
-                        placeholder="გათავისუფლების დრო"
-                        type="date"
-                    />
-                </div>
-            </div>
+      <div className="w-full px-20 py-4 flex flex-col gap-8">
+        <div className="flex justify-between w-full">
+          <h1 className="text-[#1976D2] font-medium text-[23px]">
+            თანამშრომლის დამატება/ცვლილება
+          </h1>
+          <button className="bg-[#1976D2] px-7 py-4 rounded-2xl">
+            <img src={CloseIcon} />
+          </button>
         </div>
+        <div className="flex justify-end gap-4">
+          <button className="bg-[#5CB85C] text-white px-4 py-2 rounded-md flex items-center gap-2">
+            <img src={NewIcon} />
+            New
+          </button>
+          <button className="bg-[#1976D2] text-white px-4 py-2 rounded-md flex items-center gap-2">
+            <img src={EditIcon} />
+            Edit
+          </button>
+          <button className="bg-[#D9534F] text-white px-4 py-2 rounded-md flex items-center gap-2">
+            <img src={DeleteIcon} />
+            Delete
+          </button>
+          <button className="bg-[#FBD15B] text-white px-4 py-2 rounded-md flex items-center gap-2">
+            <img src={SaveIcon} />
+            Save
+          </button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between gap-8">
+            <InputGroup
+              label="სახელი/გვარი"
+              name="fullname"
+              placeholder="სახელი/გვარი"
+              type="text"
+            />
+            <InputGroup
+              label="პირადი ნომერი / ID"
+              name="personal_id"
+              placeholder="პირადი ნომერი"
+              type="number"
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <InputGroup
+              label="ტელეფონის ნომერი"
+              name="phone_number"
+              placeholder="ტელეფონის ნომერი"
+              type="number"
+            />
+            <SelectGroup
+              label="დეპარტამენტი"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <InputGroup
+              label="დაწყების დრო"
+              name="start_date"
+              placeholder="დაწყების დრო"
+              type="date"
+            />
+            <InputGroup
+              label="გათავისუფლების დრო"
+              name="end_date"
+              placeholder="გათავისუფლების დრო"
+              type="date"
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <SelectGroup
+              label="პოზიცია"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+            <SelectGroup
+              label="ჯგუფი"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <SelectGroup
+              label="განრიგი"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+            <SelectGroup
+              label="საპატიო წუთები დღეში"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <SelectGroup
+              label="აირჩიეთ მოწყობილობა"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+            <SelectGroup
+              label="ბარათის ნომერი"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+          </div>
+          <div className="flex justify-between gap-8">
+            <InputGroup
+              label="საკონტროლო ჯამი"
+              name="fullname"
+              placeholder="საკონტროლო ჯამი"
+              type="text"
+            />
+            <SelectGroup
+              label="სტატუსი"
+              options={["Option 1", "Option 2", "Option 3"]}
+            />
+          </div>
+        </div>
+      </div>
     </AuthenticatedLayout>
-  )
+  );
 }
 
 export default EmployeeCreate
